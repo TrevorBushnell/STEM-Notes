35564bc0479b35e446b8f0be1208d66d9dd7ab18
TrevorBushnell <tbushnell11@gmail.com>
2022-09-23 08:48:42 -0700

    lecture notes CPSC436 9/23/22

---
+++ MATH425 Applied Statistical Models/r_scripts/skin_cancer.r
@@ -0,0 +1,16 @@
+# load the dataset
+data = read.table('/home/tbushnell/GitHub/STEM-Notes/MATH425 Applied Statistical Models/data_files/skin_cancer_data.txt', header=T)
+x = data$Lat
+y = data$Mort
+
+# fit a simple linear regression
+data_reg = lm(x ~ y)
+summary(data_reg)
+
+# display scatterplot of the data w/ abline
+plot(x, y, xlab='Latitude', ylab='Mort', main='Latitude VS Mort')
+abline(data_reg)
+
+# display residual plot with fitted values
+plot(x=fitted(data_reg), y=resid(data_reg), xlab='fitted values', ylab='residuals', main='residual plot for the skin cancer data')
+abline(0,0)
